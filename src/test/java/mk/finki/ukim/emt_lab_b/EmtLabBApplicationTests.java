f4f8ad9454c53631bde9650aadc88757fe6eda09
akolovska <ana.kolovska@students.finki.ukim.mk>
2026-06-14 14:18:59 +0200

    fsd

--- src/test/java/mk/finki/ukim/emt_lab_b/EmtLabBApplicationTests.java
+++ src/test/java/mk/finki/ukim/emt_lab_b/EmtLabBApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EmtLabBApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
