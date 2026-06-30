75ce1d7f3e304b122fb303ba45c2b5863e18e136
AnaKolovska <kolovskaana8@gmail.com>
2026-06-30 02:03:11 +0200

    update

--- frontend/src/axios/axios.ts
+++ frontend/src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ?? 'http://accommodations.local/api',
+    baseURL: import.meta.env.VITE_API_URL ?? 'http://accommodations.local:8080/api',
     headers: {
         'Content-Type': 'application/json'
     }
